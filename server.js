35c97d8be5e8fbfbb8461923c11ea1d72a0070bd
Pratikmaurya <pratikmaurya97@gmail.com>
2025-09-19 12:05:24 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const sql = require("mssql");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const connectionString = process.env.DB_CONNECTION;
+
+app.get("/", async (req, res) => {
+  if (!connectionString) {
+    return res.status(500).send(" ❌ DB_CONNECTION not set");
+  }
+  try {
+    let pool = await sql.connect(connectionString);
+    let result = await pool.request().query("SELECT GETDATE() as CurrentTime");
+    res.send(`
+      <h1>Azure Web App Connected to SQL Database  ✅ </h1>
+      <p>Database Time: ${result.recordset[0].CurrentTime}</p>
+      <p>Web App: ${process.env.WEBSITE_SITE_NAME}</p>
+    `);
+  } catch (err) {
+    res.status(500).send(" ❌ DB Connection Failed: " + err.message);
+  }
+});
+
+app.listen(port, () => console.log(`App running on port ${port}`));
